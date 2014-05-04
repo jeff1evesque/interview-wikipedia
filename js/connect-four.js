603c7b5c170e06ed6a42f9803b172066384bf336
Jeffrey <jeffrey@Chris-McFarlands-MacBook-Pro-15.local>
2014-05-03 23:35:38 -0400

    #3: add initial javascript skeleton (js)

---
+++ js/connect-four.js
@@ -0,0 +1,18 @@
+/**
+ * connect-four.js
+ *
+ * Jeffrey M. Levesque
+ * 05/03/2014
+ *
+ * This file contains the logic component for the 'connect-four' game'
+ */
+
+// variables & arrays
+
+// setting up grid
+
+// button actions
+
+// algorithm
+
+
