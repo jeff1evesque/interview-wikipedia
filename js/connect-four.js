80d59709f78eeff958e9d5f98acb2642ea7ebdff
Jeffrey <jeffrey@Chris-McFarlands-MacBook-Pro-15.local>
2014-05-04 14:57:13 -0400

    #17: declare needed variables

--- js/connect-four.js
+++ js/connect-four.js
@@ -10,6 +10,22 @@
 $( document ).ready(function() {
 
 // variables & arrays
+// variables & arrays
+var num_cells_x = 8; var num_cells_y = 7;
+var canvas_width = 480; var canvas_height = 420; canvas_cell = canvas_cell = 60;
+
+var rowLevel = new Array(num_cells_x);
+
+var arrBoard = new Array(num_cells_x);
+for(i = 0; i < arrBoard.length; i++) {
+    arrBoard[i] = new Array(num_cells_y);
+    for(j = 0; j < arrBoard[num_cells_y]; j++) {
+        arrBoard[i][j] = null;
+    }
+}
+    
+var arrRow = new Array(num_cells_y);
+flag_player = false; // indicate current player
 
 // setting up grid
 
